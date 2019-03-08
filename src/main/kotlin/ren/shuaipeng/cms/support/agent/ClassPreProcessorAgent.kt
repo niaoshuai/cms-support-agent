33227e57393d6bb72beeed69c54b6964996b9811
niaoshuai <niao.shuai123@163.com>
2019-03-08 11:04:11 +0800

    Update Maven Project

--- src/main/kotlin/ren/shuaipeng/cms/support/agent/ClassPreProcessorAgent.kt
+++ src/main/kotlin/ren/shuaipeng/cms/support/agent/ClassPreProcessorAgent.kt
@@ -1,5 +1,7 @@
 package ren.shuaipeng.cms.support.agent
 
+import org.springframework.asm.ClassReader
+import org.springframework.cglib.core.ClassNameReader
 import java.lang.instrument.ClassFileTransformer
 import java.security.ProtectionDomain
 
@@ -14,6 +16,10 @@ class ClassPreProcessorAgent : ClassFileTransformer {
         if (className!!.startsWith("ren/shuaipeng/cms")) {
             println("className = ${className}")
             // TODO 监控URL(Handler)的调用情况
+            var classReader = ClassReader(className)
+            var classNameReader = ClassNameReader.getClassName(classReader)
+            println("classNameReader = ${classNameReader}")
+
         }
         return null
     }
